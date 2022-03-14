add7690fdbfc47014302e6f66824591d14a952db
lucuma27 <yosoy2509@gmail.com>
2022-03-14 17:12:28 -0300

    Agregando arrays

--- desafio1.js
+++ desafio1.js
@@ -34,8 +34,22 @@ function peso2() {
  Por favor responder con el número correspondiente: servicio digital (1)/ compra de producto en el exterior (2)`) 
 
  let moneda = prompt (`¿En que moneda se encuentra el consumo? Responder: peso/dolar`)
- let valor = prompt ("¿Cuál es el valor?")
 
+ let array = prompt("¿Cuantos son los valores?")
+
+ const precios = [];
+
+ do {
+    let valor = prompt ("¿Cuáles son los precios?");
+    precios.push(valor);
+
+
+ } while (precios.length!= array);
+
+
+ prompt (`Para continuar, ¿Nos confirmas si los precios que nos indicaste son correctos? 
+ ${precios}      
+ Porfavor responder si/no`)
 
 if ((moneda == "dolar") && (saludo == "1")) {
     dolarpeso()
